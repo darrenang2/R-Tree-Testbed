a621f0b0159315fa25cf9403e2aa161e0ff925e4
darrenang2 <darrenang2001@gmail.com>
2024-05-14 14:09:40 -0700

    insert function for leaves implemented

--- src/krnl.cpp
+++ src/krnl.cpp
@@ -25,14 +25,14 @@ struct Node
     int RminY;
     int RmaxY;
 
-    int Lindex;
-    int Rindex;
+    int Lindex = -1;
+    int Rindex = -1;
 
     Node(bool l, int x1, int x2, int y1, int y2)
         : leaf(l), minX(x1), maxX(x2), minY(y1), maxY(y2) {}
 
-    Node(int lx1, int lx2, int ly1, int ly2, int rx1, int rx2, int ry1, int ry2, int li, int ri)
-        : LminX(lx1), LmaxX(lx2), LminY(ly1), LmaxY(ly2), RminX(rx1), RmaxX(rx2), RminY(ry1), RmaxY(ry2), Lindex(li), Rindex(ri) {}
+    Node(bool l, int lx1, int lx2, int ly1, int ly2, int rx1, int rx2, int ry1, int ry2, int li, int ri)
+        : leaf(l), LminX(lx1), LmaxX(lx2), LminY(ly1), LmaxY(ly2), RminX(rx1), RmaxX(rx2), RminY(ry1), RmaxY(ry2), Lindex(li), Rindex(ri) {}
 
     Node()
         : leaf(false) {}
@@ -46,13 +46,21 @@ struct Node
     {
         return (node1.minX <= node2.maxX && node1.maxX >= node2.minX && node1.minY <= node2.maxY && node1.maxY >= node2.minY);
     }
+
+    Node leafToParent(Node node)
+    {
+        node = Node(0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1);
+        return node;
+    }
 };
 
 /*==================================================== INITIALIZE DATA ===============================================================*/
 
 #define MAX 100             // Max number of nodes
 #define INT_MAX 2147483647; // Max integer value
+#define MAX_LEAVES 2;       // Max number of leaves in a node
 
+int currNumNodes = 7; // Number of Nodes
 const int numNodes = MAX;
 Node nodes[numNodes];
 
@@ -189,9 +197,9 @@ void split(int nodeIndex, Node currentNode, Node newNode)
     // Choose the best pair of nodes to split
     int bestPair[2];
     int bestArea = INT_MAX;
-    for (int i = 0; i < numNodes; i++)
+    for (int i = 0; i < currNumNodes; i++)
     {
-        for (int j = i + 1; j < numNodes; j++)
+        for (int j = i + 1; j < currNumNodes; j++)
         {
             int area = (nodes[i].maxX - nodes[i].minX) * (nodes[i].maxY - nodes[i].minY) + (nodes[j].maxX - nodes[j].minX) * (nodes[j].maxY - nodes[j].minY);
             if (area < bestArea)
@@ -233,38 +241,71 @@ void split(int nodeIndex, Node currentNode, Node newNode)
 
 /*==================================================== INSERTION FUNCTIONS ===============================================================*/
 
-void insertLeaf(Node node, int nodeIndex)
+void insert(bool leaf, data_t minX, data_t maxX, data_t minY, data_t maxY)
 {
-    nodes[nodeIndex + 1] = node;
-    // update previous nodes to have the new node as a child
-    Node currentNode = nodes[nodeIndex];
+    Node newNode = Node(false, minX, maxX, minY, maxY);
+    // for (int i = 0; i < currNumNodes; i++)
+    int count = 0;
+    push(count); // Start with the root node index
 
-    currentNode.leaf = false;
-    currentNode.Lindex = nodeIndex;
-    currentNode.Rindex = nodeIndex + 1;
-    nodes[nodeIndex] = currentNode;
-
-}
-
-void insert(data_t minX, data_t maxX, data_t minY, data_t maxY)
-{
-    Node node = Node(false, minX, maxX, minY, maxY);
-    sortByArea();
-    push(0); // Start with the root node index
-    int nodeIndex = pop();
-    Node currentNode = nodes[nodeIndex];
-
-    while (!isEmpty)
+    do
     {
-        if (currentNode.area() < node.area())
+        int nodeIndex = pop();
+        Node currentNode = nodes[nodeIndex];
+        Node currentNodeParent;
+
+        // if is leaf (Lindex = -1)
+        // insert node into Lindex
+        // else if Rindex = -1
+        // insert node into Rindex
+        // else
+        // insert into stack (choose subtree)
+        // update left index / right index of parent node
+
+        if (currentNode.leaf)
         {
-            split(nodeIndex, currentNode, node);
+            std::cout << "Adding left leaf" << std::endl;
+            currentNodeParent = currentNode.leafToParent(currentNode);
+            newNode.leaf = true;
+
+            currentNodeParent.Lindex = currNumNodes;
+            nodes[currNumNodes] = newNode; // currNumNodes = array index of newest node, gets updated everytime a node is added
+            currNumNodes++;
+
+            currentNodeParent.LminX = newNode.minX;
+            currentNodeParent.LmaxX = newNode.maxX;
+            currentNodeParent.LminY = newNode.minY;
+            currentNodeParent.LmaxY = newNode.maxY;
+
+            nodes[nodeIndex] = currentNodeParent;
+        }
+        else if (currentNode.Rindex == -1)
+        {
+            std::cout << "Adding right leaf" << std::endl;
+            newNode.leaf = 1;
+
+            currentNode.Rindex = currNumNodes;
+            nodes[currNumNodes] = newNode;
+            currNumNodes++;
+
+            currentNode.RminX = newNode.minX;
+            currentNode.RmaxX = newNode.maxX;
+            currentNode.RminY = newNode.minY;
+            currentNode.RmaxY = newNode.maxY;
+
+            nodes[nodeIndex] = currentNode;
         }
         else
         {
-            insertLeaf(node, nodeIndex);
+            push(++count);
+            // chooseSubTree();
         }
-    }
+
+        // if leaves > max
+        // overFlowTreatment();
+
+        // sortByArea();
+    } while (!isEmpty() && count < MAX);
 }
 
 /*==================================================== DELETION FUNCTIONS ===============================================================*/
@@ -273,17 +314,16 @@ void insert(data_t minX, data_t maxX, data_t minY, data_t maxY)
 // {
 // }
 
-
 /*==================================================== KERNEL ===============================================================*/
 
-extern "C" void krnl(data_t minX, data_t maxX, data_t minY, data_t maxY, data_t *output, data_t newNode)
+extern "C" void krnl(data_t minX, data_t maxX, data_t minY, data_t maxY, data_t *output)
 {
 #pragma HLS INTERFACE mode = m_axi port = output
 
     // L:(x1 -> x2) (y1 -> y2), R:(x1 -> x2) (y1 -> y2), Child L, Child R
-    nodes[0] = Node(0, 9, 0, 10, 11, 19, 11, 19, 1, 2);
-    nodes[1] = Node(0, 4, 0, 4, 5, 9, 5, 9, 3, 4);
-    nodes[2] = Node(10, 14, 10, 14, 15, 19, 15, 19, 5, 6);
+    nodes[0] = Node(false, 0, 9, 0, 10, 11, 19, 11, 19, 1, 2);
+    nodes[1] = Node(false, 0, 4, 0, 4, 5, 9, 5, 9, 3, 4);
+    nodes[2] = Node(false, 10, 14, 10, 14, 15, 19, 15, 19, 5, 6);
     // Leaf = true, (x1 -> x2), (y1 -> y2)
     nodes[3] = Node(true, 0, 4, 0, 4);
     nodes[4] = Node(true, 5, 9, 5, 9);
@@ -291,11 +331,21 @@ extern "C" void krnl(data_t minX, data_t maxX, data_t minY, data_t maxY, data_t 
     nodes[6] = Node(true, 15, 19, 15, 19);
 
     // search(minX, maxY, minY, maxY, output);
-    
-    insert(1, 3, 1, 3);
-    for (int i = 0; i < numNodes; i++)
+
+    insert(false, 1, 3, 1, 3);
+    insert(false, 3, 4, 3, 4);
+    insert(false, 6, 9, 6, 9);
+
+    for (int i = 0; i < currNumNodes; i++)
     {
-        std::cout << "Node " << i << ": " << nodes[i].minX << "->" << nodes[i].maxX << " " << nodes[i].minY << "->" << nodes[i].maxY << std::endl;
+        if (!nodes[i].leaf)
+        {
+            std::cout << "Node " << i << ": " << nodes[i].LminX << "->" << nodes[i].LmaxX << " " << nodes[i].LminY << "->" << nodes[i].LmaxY << " " << nodes[i].RminX 
+            << "->" << nodes[i].RmaxX << " " << nodes[i].RminY << "->" << nodes[i].RmaxY << " L index:" << nodes[i].Lindex << " R index:" << nodes[i].Rindex << std::endl;
+        }
+        else
+        {
+            std::cout << "Node " << i << ": " << nodes[i].minX << "->" << nodes[i].maxX << " " << nodes[i].minY << "->" << nodes[i].maxY << std::endl;
+        }
     }
 }
-
