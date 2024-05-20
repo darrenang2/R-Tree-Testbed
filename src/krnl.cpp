29b364f1b52c3376f86db7473dc6747dfce577d3
darrenang2 <darrenang2001@gmail.com>
2024-05-20 15:24:55 -0700

    separate sortByAreaEnlargement and sortByOverlapEnlargement functions

--- src/krnl.cpp
+++ src/krnl.cpp
@@ -181,89 +181,11 @@ int search(data_t minX, data_t maxX, data_t minY, data_t maxY, data_t *output)
 
 /*==================================================== SPLIT FUNCTIONS ===============================================================*/
 
-int sortedArray[MAX];
-
-void sortByArea()
-{
-    for (int i = 0; i < numNodes; i++)
-    {
-        sortedArray[i] = nodes[i].area();
-    }
-
-    int i, j;
-    bool swapped;
-    for (i = 0; i < numNodes - 1; i++)
-    {
-        swapped = false;
-        for (j = 0; j < numNodes - i - 1; j++)
-        {
-            if (sortedArray[j] > sortedArray[j + 1])
-            {
-                std::swap(sortedArray[j], sortedArray[j + 1]);
-                swapped = true;
-            }
-        }
-        // If no two elements were swapped by inner loop, then break
-        if (swapped == false)
-            break;
-    }
-
-    for (int i = 0; i < numNodes; i++)
-    {
-        nodes[i] = nodes[sortedArray[i]];
-    }
-}
-
-void split(int nodeIndex, Node currentNode, Node newNode)
-{
-    // Choose the best pair of nodes to split
-    int bestPair[2];
-    int bestArea = INT_MAX;
-    for (int i = 0; i < currNumNodes; i++)
-    {
-        for (int j = i + 1; j < currNumNodes; j++)
-        {
-            int area = (nodes[i].maxX - nodes[i].minX) * (nodes[i].maxY - nodes[i].minY) + (nodes[j].maxX - nodes[j].minX) * (nodes[j].maxY - nodes[j].minY);
-            if (area < bestArea)
-            {
-                bestArea = area;
-                bestPair[0] = i;
-                bestPair[1] = j;
-            }
-        }
-    }
-
-    // Split the best pair of nodes
-    Node L = nodes[bestPair[0]];
-    Node R = nodes[bestPair[1]];
-
-    // Update the current node
-    currentNode.LminX = L.minX;
-    currentNode.LmaxX = L.maxX;
-    currentNode.LminY = L.minY;
-    currentNode.LmaxY = L.maxY;
-    currentNode.RminX = R.minX;
-    currentNode.RmaxX = R.maxX;
-    currentNode.RminY = R.minY;
-    currentNode.RmaxY = R.maxY;
-    currentNode.Lindex = bestPair[0];
-    currentNode.Rindex = bestPair[1];
-    currentNode.leaf = false;
-    nodes[nodeIndex] = currentNode;
-
-    // Update the nodes array
-    for (int i = 0; i < numNodes; i++)
-    {
-        if (i != bestPair[0] && i != bestPair[1])
-        {
-            nodes[i] = nodes[i + 1];
-        }
-    }
-}
+// void splitNode(Node node)
 
 /*==================================================== INSERTION FUNCTIONS ===============================================================*/
 
-// int sortedArray[MAX];
+int sortedArray[MAX];
 
 void sortByAreaEnlargement(Node newNode)
 {
@@ -276,16 +198,9 @@ void sortByAreaEnlargement(Node newNode)
         int nodeIndex = pop();
         Node currentNode = nodes[nodeIndex];
 
-        boundingBox box = calculateBoundingBox(currentNode);
-        if (box.maxX >= newNode.maxX && box.minX <= newNode.minX && box.maxY >= newNode.maxY && box.minY <= newNode.minY)
+        if (currentNode.leaf)
         {
-            // new node is within the bounding box
-            push(currentNode.Lindex);
-            push(currentNode.Rindex);
-        }
-        else if (currentNode.leaf)
-        {
-            // new node is leaf
+            // the node being compared to new node is leaf
             area_maxX = std::max(currentNode.maxX, newNode.maxX);
             area_maxY = std::max(currentNode.maxY, newNode.maxY);
             area_minX = std::min(currentNode.minX, newNode.minX);
@@ -297,15 +212,80 @@ void sortByAreaEnlargement(Node newNode)
         }
         else
         {
-            // new node is not within the bounding box
-            area_maxX = std::max(box.maxX, newNode.maxX);
-            area_maxY = std::max(box.maxY, newNode.maxY);
-            area_minX = std::min(box.minX, newNode.minX);
-            area_minY = std::min(box.minY, newNode.minY);
+            boundingBox box = calculateBoundingBox(currentNode);
+            if (box.maxX >= newNode.maxX && box.minX <= newNode.minX && box.maxY >= newNode.maxY && box.minY <= newNode.minY)
+            {
+                // new node is within the bounding box
+                push(currentNode.Lindex);
+                push(currentNode.Rindex);
+            }
+            else
+            {
+                // new node is not within the bounding box
+                area_maxX = std::max(box.maxX, newNode.maxX);
+                area_maxY = std::max(box.maxY, newNode.maxY);
+                area_minX = std::min(box.minX, newNode.minX);
+                area_minY = std::min(box.minY, newNode.minY);
 
-            area_enlargement = (area_maxX - area_minX) * (area_maxY - area_minY) - currentNode.area();
-            std::cout << "Area Enlargement: " << area_enlargement << " Node: " << nodeIndex << std::endl;
+                area_enlargement = (area_maxX - area_minX) * (area_maxY - area_minY) - currentNode.area();
+                std::cout << "Area Enlargement: " << area_enlargement << " Node: " << nodeIndex << std::endl;
+                sortedArray[i++] = nodeIndex;
+            }
+        }
+    }
+}
+
+void sortByOverlapEnlargement(Node newNode)
+{
+    int i = 0;
+    int maxX, maxY, minX, minY;
+    push(0);
+    while (!isEmpty())
+    {
+        int nodeIndex = pop();
+        Node currentNode = nodes[nodeIndex];
+
+        if (currentNode.leaf)
+        {
+            // the node being compared to new node is leaf
+            maxX = std::min(currentNode.maxX, newNode.maxX);
+            maxY = std::min(currentNode.maxY, newNode.maxY);
+            minX = std::max(currentNode.minX, newNode.minX);
+            minY = std::max(currentNode.minY, newNode.minY);
+
+            int overlap = (maxX - minX) * (maxY - minY);
+            std::cout << "Overlap: " << overlap << " Leaf Node: " << nodeIndex << std::endl;
             sortedArray[i++] = nodeIndex;
+        }
+        else
+        {
+            // overlap of left child
+            maxX = std::min(currentNode.LmaxX, newNode.maxX);
+            maxY = std::min(currentNode.LmaxY, newNode.maxY);
+            minX = std::max(currentNode.LminX, newNode.minX);
+            minY = std::max(currentNode.LminY, newNode.minY);
+            int leftOverlap = (maxX - minX) * (maxY - minY);
+            std::cout << "Left Overlap: " << leftOverlap << " Node: " << nodeIndex << std::endl;
+
+            // overlap of right child
+            maxX = std::min(currentNode.RmaxX, newNode.maxX);
+            maxY = std::min(currentNode.RmaxY, newNode.maxY);
+            minX = std::max(currentNode.RminX, newNode.minX);
+            minY = std::max(currentNode.RminY, newNode.minY);
+            int rightOverlap = (maxX - minX) * (maxY - minY);
+            std::cout << "Right Overlap: " << rightOverlap << " Node: " << nodeIndex << std::endl;
+
+            // push node with least overlap
+            if (leftOverlap < rightOverlap)
+            {
+                push(currentNode.Lindex);
+                sortedArray[i++] = currentNode.Lindex;
+            }
+            else
+            {
+                push(currentNode.Rindex);
+                sortedArray[i++] = currentNode.Rindex;
+            }
         }
     }
 }
@@ -390,7 +370,7 @@ extern "C" void krnl(data_t minX, data_t maxX, data_t minY, data_t maxY, data_t 
 #pragma HLS INTERFACE mode = m_axi port = output
 
     // L:(x1 -> x2) (y1 -> y2), R:(x1 -> x2) (y1 -> y2), Child L, Child R
-    nodes[0] = Node(false, 0, 9, 0, 10, 11, 19, 11, 19, 1, 2);
+    nodes[0] = Node(false, 0, 9, 0, 9, 10, 19, 10, 19, 1, 2);
     nodes[1] = Node(false, 0, 4, 0, 4, 5, 9, 5, 9, 3, 4);
     nodes[2] = Node(false, 10, 14, 10, 14, 15, 19, 15, 19, 5, 6);
     // Leaf = true, (x1 -> x2), (y1 -> y2)
@@ -419,7 +399,7 @@ extern "C" void krnl(data_t minX, data_t maxX, data_t minY, data_t maxY, data_t 
     // }
 
     nodes[7] = Node(false, 0, 7, 0, 7);
-    sortByAreaEnlargement(nodes[7]);
+    sortByOverlapEnlargement(nodes[7]);
     std::cout << "Sorted Array: ";
     for (int i = 0; i < currNumNodes; i++)
     {
